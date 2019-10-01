a9ef0496ada12fefea22e46f7923b71cb90fe3d0
combatd <combatdstar@gmail.com>
2019-10-01 13:26:06 -0700

    created buttons, implemented class Tomagotchi instance variables

---
+++ js/app.js
@@ -0,0 +1,18 @@
+class Tomagotchi {
+    constructor() {
+        this.name = name;
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.boredom = 1;
+        this.age = 0;
+    }
+    // add the ability to set your pet's name
+    namePet(name) {
+       this.name = name;
+    }
+    // toggle the lighting of the page
+    toggleLights() {
+        
+    }
+
+}
