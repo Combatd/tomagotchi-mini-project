5ef30171d55d14ee644310c6598cc02f9e1edaf4
combatd <combatdstar@gmail.com>
2019-10-01 16:11:21 -0700

    class Game created

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// avoid having multiple setInterval() calls
+
 class Tomagotchi {
     constructor() {
         this.name = name;
@@ -12,7 +14,34 @@ class Tomagotchi {
     }
     // toggle the lighting of the page
     toggleLights() {
+        const $tomagotchi = $('#tomagotchi');
+        
+        if ($tomagotchi.attr('backgroundColor', 'black')) {
+            $tomagotchi.attr('backgroundColor', 'white');
+        } else {
+            $tomagotchi.attr('backgroundColor', 'black');
+        }      
         
     }
-
+    playPet() {
+        this,boredom -= Math.floor(Math.random() * Math.floor(5));
+    }
+}
+// Tomagotchi is the player to pass in
+class Game {
+    constructor(tomagotchi) {
+        this.tomagotchi = tomagotchi;
+    }
+    start() {
+        Tomagotchi();
+    }
+    isGameOver(tomagotchi) {
+        if (tomagotchi.hunger > 9) {
+            console.log(`${tomagotchi.name} is deceased due to hunger!`);
+        } else if (tomagotchi.sleepiness > 9) {
+            console.log(`${tomagotchi.name} is deceased due to exhaustion!`);
+        } else if (tomagotchi.boredom > 9) {
+            console.log(`${tomagotchi.name} is deceased due to boredom!`);
+        }
+    }
 }
