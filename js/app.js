1110f5641bee8bcc9f01ebadd213a3fe37ab78a7
combatd <combatdstar@gmail.com>
2019-10-02 09:21:35 -0700

    setInterval in start() function now has a clearInterval(id)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // avoid having multiple setInterval() calls
 
 class Tomagotchi {
-    constructor() {
+    constructor(name) {
         this.name = name;
         this.hunger = 1;
         this.sleepiness = 1;
@@ -12,7 +12,7 @@ class Tomagotchi {
     namePet(name) {
        this.name = name;
     }
-    // toggle the lighting of the page
+    // toggle the lighting of the page (sleeping)
     toggleLights() {
         const $tomagotchi = $('#tomagotchi');
         
@@ -30,25 +30,37 @@ class Tomagotchi {
 // Tomagotchi is the player to pass in
 const game = {
     start() {
-        Tomagotchi();
-        setInterval(function () {
-            this.increaseAge(tomagotchi)
+        const tomagotchi = new Tomagotchi("Broly");
+        console.log(tomagotchi);
+        const intervalId = setInterval(function () {
+            game.increaseAge(tomagotchi);
+            console.log()
+            if (game.isGameOver(tomagotchi) === "gameover") {
+                clearInterval(intervalId) // end the setInterval
+            }; // check isGameOver
         }, 1000) 
     },
     // You pet should die if Hunger, Boredom, or Sleepiness hits 10.
     isGameOver(tomagotchi) {
         if (tomagotchi.hunger > 9) {
             console.log(`${tomagotchi.name} is deceased due to hunger!`);
+            return "gameover";
         } else if (tomagotchi.sleepiness > 9) {
             console.log(`${tomagotchi.name} is deceased due to exhaustion!`);
+            return "gameover";
         } else if (tomagotchi.boredom > 9) {
             console.log(`${tomagotchi.name} is deceased due to boredom!`);
+            return "gameover";
         }
     },
     // Increase your pet's age every x minutes
     increaseAge(tomagotchi) {
-        
+        tomagotchi.age++
         // Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-
+        tomagotchi.hunger+= Math.floor(Math.random() * Math.floor(3));
+        tomagotchi.sleepiness+=Math.floor(Math.random() * Math.floor(3));
+        tomagotchi.boredom += Math.floor(Math.random() * Math.floor(3));
     }
 }
+
+game.start();
